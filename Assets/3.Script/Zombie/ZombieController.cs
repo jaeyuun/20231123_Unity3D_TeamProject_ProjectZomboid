9df4670609be0a4819a40dd75d6e8f1dd7dad866
Jaeyun Lee <ghghwodbs@gmail.com>
2023-11-07 17:53:49 +0900

    [Feat] Zombie Spawner, Zombie Data Plus
    1. Zombie Spawner 추가
    2. Zombie Data를 이용해 랜덤 좀비 생성
    3. Zombie Random Target 수정 필요

--- Assets/3.Script/Zombie/ZombieController.cs
+++ Assets/3.Script/Zombie/ZombieController.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+public enum ZombieAudio
+{
+    Hit = 0,
+    Dead,
+    Walk,
+}
+
 public class ZombieController : MonoBehaviour
 {
     // Zombie NavMesh
@@ -14,23 +21,30 @@ public class ZombieController : MonoBehaviour
     // RandomTarget NavMesh
     private float range = 10f;
     private Vector3 point;
+    public bool isNonTarget = true; // player가 target이 아닐 때
 
     private Animator zombieAnim;
     [SerializeField] private GameObject screamRange;
-    [SerializeField] private bool isScreamZombie = false;
+    private bool isScreamZombie = false;
+
+    [Header("효과음")]
+    private AudioSource zombieAudio;
+    [SerializeField] private AudioClip[] audioClip;
+
+    // ZombieData
+    private SkinnedMeshRenderer skinned;
 
     private void Awake()
     {
         TryGetComponent(out nav);
         TryGetComponent(out zombieAnim);
-
-        if (GameObject.FindGameObjectWithTag("Player"))
+        TryGetComponent(out zombieAudio);
+        if (GameObject.FindGameObjectWithTag("Player") != null)
         {
             player = GameObject.FindGameObjectWithTag("Player").transform;
         }
         randomTarget = GameObject.FindGameObjectWithTag("RandomTarget").transform;
-
-        // isScreamZombie = zombieData; zombie 종류에 따라 불러오기... todo
+        skinned = transform.GetChild(0).GetComponent<SkinnedMeshRenderer>();
     }
 
     private void Start()
@@ -43,6 +57,13 @@ public class ZombieController : MonoBehaviour
         ZombieTransToPlayer();
     }
 
+    public void SetUp(ZombieData data)
+    {
+        // ZombieDataSetUp
+        skinned.sharedMesh = data.skinnedMesh;
+        isScreamZombie = data.isScreamZombie;
+    }
+
     private void ZombieTransToPlayer()
     {
         // target에 따른 네비 적용
@@ -52,17 +73,21 @@ public class ZombieController : MonoBehaviour
     private IEnumerator RandomTargetPos_Co()
     {
         // random target position select
-        if (RandomPoint(randomTarget.position, range, out point))
+        if (isNonTarget)
         {
-            randomTarget.position = point;
-        }
-        targetPos = randomTarget;
-        if (Vector3.Distance(randomTarget.position, transform.position) <= 1.0f)
-        {
-            zombieAnim.SetBool("isIdle", true);
-        } else
-        {
-            zombieAnim.SetBool("isIdle", false);
+            if (RandomPoint(randomTarget.position, range, out point))
+            {
+                randomTarget.position = point;
+            }
+            targetPos = randomTarget;
+            if (Vector3.Distance(randomTarget.position, transform.position) <= nav.stoppingDistance + 1.0f)
+            {
+                zombieAnim.SetBool("isIdle", true);
+            }
+            else
+            {
+                zombieAnim.SetBool("isIdle", false);
+            }
         }
         yield return new WaitForSeconds(5f);
         StartCoroutine(RandomTargetPos_Co());
@@ -70,7 +95,7 @@ public class ZombieController : MonoBehaviour
 
     private bool RandomPoint(Vector3 center, float range, out Vector3 result)
     {
-        // 플레이어 범위 밖일 때 랜덤 타겟
+        // 플레이어 범위 밖일 때 랜덤 타겟 위치 설정, object 아닌 position으로만 따라갈 수 있도록 설정... todo
         for (int i = 0; i < 30; i++)
         {
             Vector3 randomPoint = center + Random.insideUnitSphere * range;
@@ -89,11 +114,19 @@ public class ZombieController : MonoBehaviour
     {
         if (other.CompareTag("Sound"))
         {
+            isNonTarget = false;
+            targetPos = player;
             zombieAnim.SetBool("isPlayerFind", true);
             if (isScreamZombie && Vector3.Distance(player.position, transform.position) > 1.5f)
             {
                 StartCoroutine(ZombieScream_Co());
             }
+        }
+        if (other.CompareTag("Attack"))
+        {
+            // zombie Hit method... todo
+            zombieAnim.SetTrigger("isDamage");
+            zombieAudio.PlayOneShot(audioClip[(int)ZombieAudio.Hit]);
         }
     }
 
@@ -120,6 +153,7 @@ public class ZombieController : MonoBehaviour
         if (other.CompareTag("Sound"))
         {
             zombieAnim.SetBool("isPlayerFind", false);
+            isNonTarget = true;
         }
     }
 
