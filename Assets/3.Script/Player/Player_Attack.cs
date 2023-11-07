e2639146dcf8aac31d89ea8828565c2d2332e9b3
BANIKIM <72877425+BANIKIM@users.noreply.github.com>
2023-11-07 16:21:09 +0900

    [Feat] Hit시 피나오게 함

--- Assets/3.Script/Player/Player_Attack.cs
+++ Assets/3.Script/Player/Player_Attack.cs
@@ -4,24 +4,31 @@ using UnityEngine;
 
 public class Player_Attack : MonoBehaviour
 {
+    [Header("애니메이션 컨트롤러")]
     public Animator anim;
+    [Header("공격판정")]
     public bool isAttack;//판정을 위한 bool값
+    [Header("스윙소리")]
     public AudioClip BatSwing;
     private AudioSource audioSource;
+    [Header("플레이어오브젝트")]
     [SerializeField] private Gun_Shot gun_Shot;
+    [Header("건사운드오브젝트")]
     public GameObject Sound_Gun;
+    private bool IsMovement=false;
 
+
+    [Header("엑티브할 베트들")]
     //등에 있는 배트
     [SerializeField] private GameObject Bat_Spine;
-    private bool Bat_In=true;
-    
+    private bool Bat_In=true;    
     //손에 있는 배트
     [SerializeField] private GameObject Bat_Hand;
     private bool Bat_Out;
 
 
-    public bool Bat_Get;//배트를 가지고 있냐?
-    
+    [Header("테스트를위한")]
+    public bool Bat_Get;//배트를 가지고 있냐?    
     public bool Melee_weapon;
     public bool Range_weapon;
 
@@ -35,17 +42,20 @@ public class Player_Attack : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetButtonDown("Jump"))//동작하길 바란다...Todo 필요없다면 삭제 하길...
+        if (Input.GetButtonDown("Jump")&&!IsMovement)//동작하길 바란다...Todo 필요없다면 삭제 하길...
         {
             anim.SetLayerWeight(1, 0);
             anim.SetTrigger("isKickig");
+            IsMovement = true;
+            Invoke("isMovement", 1.5f);
         }
 
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        if (Input.GetKeyDown(KeyCode.Alpha1) && !IsMovement)
         {
             anim.SetLayerWeight(1, 1);//상체 애니메이션 재생
             Debug.Log("누르는중~~~");
             Bat_take_out(Bat_Get);
+
         }
 
         if (Input.GetMouseButton(1))//우클릭시
@@ -55,23 +65,27 @@ public class Player_Attack : MonoBehaviour
             if (Melee_weapon)//근접무기를 들고 있다면
             {
                 anim.SetBool("isWeapon", true);//근접무기 대기자세를 재생하고
-                if (Input.GetMouseButtonDown(0))//좌클릭을 하면
+                if (Input.GetMouseButtonDown(0) && !IsMovement)//좌클릭을 하면
                 {
                     anim.SetTrigger("isSwing");//스윙공격을 한다
                     isAttack = true;
                     Invoke("BatSWingClip", 0.3f);
+                    IsMovement = true;
+                    Invoke("isMovement", 1f);
                 }
                 
             }
             else if (Range_weapon)
             {
                 anim.SetBool("isAiming", true);
-                if (Input.GetMouseButtonDown(0))//총쏘기
+                if (Input.GetMouseButton(0) && !IsMovement)//총쏘기
                 {
 
                     gun_Shot.ShotEvent();
                     anim.SetTrigger("isFiring");
                     Sound_Gun.SetActive(false);
+                    IsMovement = true;
+                    Invoke("isMovement", 0.3f);
                 }
             }
    
@@ -127,6 +141,10 @@ public class Player_Attack : MonoBehaviour
         Bat_Out = newBatOut;
     }
 
+    private void isMovement()//행동가능여부
+    {
+        IsMovement = false;
+    }
 
 
 }
