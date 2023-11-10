33d5ef83882fb515343d937946f789af75745621
Sujin <tnwlsqp12@gmail.com>
2023-11-10 17:45:50 +0900

    [Feat] Object Add, Object replace

--- Assets/3.Script/Player/Manager/SeletionManager.cs
+++ Assets/3.Script/Player/Manager/SeletionManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SeletionManager : MonoBehaviour
 {
     [SerializeField] private string selectable = "Selectable";
+    private float distance = 5f;
     [SerializeField] private Material highlightMaterial;
     [SerializeField] private Material defalultMateral;
 
@@ -19,14 +20,20 @@ public class SeletionManager : MonoBehaviour
             _seletion = null;
         }
 
-        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        hitObject();
+    }
+    
+    private void hitObject()
+    {
+        //var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Ray ray = new Ray(transform.position + new Vector3(0, 1.2f, 0), transform.forward);
         RaycastHit hit;
 
-        if(Physics.Raycast(ray, out hit))
+        if (Physics.Raycast(ray, out hit, distance))
         {
             // Debug.DrawRay(transform.position + new Vector3(0, 1.2f, 0), transform.forward * 10f, Color.red);
             var selection = hit.transform;
-            if(selection.CompareTag(selectable))
+            if (selection.CompareTag(selectable))
             {
                 var selectionRenderer = selection.GetComponent<Renderer>();
                 if (selectionRenderer != null)
@@ -36,10 +43,10 @@ public class SeletionManager : MonoBehaviour
                 }
                 _seletion = selection;
             }
-            
-            
+
+
         }
+
     }
-    
 
 }
