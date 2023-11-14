8dd93271cee968c956b586da41c8dd71badb6c80
BANIKIM <72877425+BANIKIM@users.noreply.github.com>
2023-11-14 16:18:25 +0900

    [Feat] 차량 도어쪽에서 탑승 추가, 차량 상태창 추가

--- Assets/3.Script/Player/Player_isCar.cs
+++ Assets/3.Script/Player/Player_isCar.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Player_isCar : MonoBehaviour
 {
     public bool iscar = false;
-    private void OnCollisionStay(Collision collision)
+    [SerializeField] private GameObject CarInfo;
+/*    private void OnCollisionStay(Collision collision)
     {
+
         if(collision.gameObject.CompareTag("Car"))
         {
-            iscar = true;
+                       
             Debug.Log("iscar");
         }
    
@@ -17,10 +19,39 @@ public class Player_isCar : MonoBehaviour
 
     private void OnCollisionExit(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Car"))
+        if (collision.gameObject.CompareTag("CarDoor"))
         {
             iscar = false;
             Debug.Log("iscar");
+        }
+    }*/
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.CompareTag("CarDoor"))
+        {
+            iscar = true;
+            Debug.Log("도어어어어");
+        }
+        if (other.gameObject.CompareTag("CarHood"))
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                CarInfo.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("CarDoor"))
+        {
+            iscar = false;
+        }
+
+        if (other.gameObject.CompareTag("CarHood"))
+        {
+            CarInfo.SetActive(false);
         }
     }
 }
