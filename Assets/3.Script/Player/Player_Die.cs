67d228375df145485fb827475d6ac9311672568d
BANIKIM <72877425+BANIKIM@users.noreply.github.com>
2023-11-14 11:53:08 +0900

    [Fix] 논타겟 예외처리 수정중

--- Assets/3.Script/Player/Player_Die.cs
+++ Assets/3.Script/Player/Player_Die.cs
@@ -12,6 +12,7 @@ public class Player_Die : MonoBehaviour
     public HitColl hitColl;
     private bool isDie=false;
     private bool Die = false;
+    //private bool NonTarget = false;
 
 
     void Update()
@@ -31,7 +32,7 @@ public class Player_Die : MonoBehaviour
         
         if(other.gameObject.CompareTag("Zombie")&& isDie)
         {
-            
+            Debug.Log("µé¾î¿È");
             other.gameObject.TryGetComponent(out ZombieController zombie);
             zombie.nonTarget = true;
         }
