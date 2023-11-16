922145324de908ad2e30975e08aa168d29f78310
Jaeyun Lee <ghghwodbs@gmail.com>
2023-11-16 14:20:14 +0900

    [Fix] Mouse img 수정, RightClick 수정중

--- Assets/3.Script/ECT/UI/RightClickMenu.cs
+++ Assets/3.Script/ECT/UI/RightClickMenu.cs
@@ -4,20 +4,21 @@ using UnityEngine.UI;
 
 public class RightClickMenu : MonoBehaviour, IPointerClickHandler, IClickState
 {
-    private GameObject rightClickMenu;
+    private GameObject rightClickMenu; // UI 보여줄 위치
     private RectTransform rightClickRect;
     private float objectDistance;
     private float minDistance = 0;
     private RaycastHit minHit;
 
-    [SerializeField] private Button buttonPrefab;
+    [SerializeField] private GameObject buttonPrefab;
     private class RightClickButton
     {
-        public Button button;
+        public GameObject button;
         public Text buttonText;
     }
     private int buttonCount = 0;
     private RightClickButton[] rightClickButtons;
+    private Transform objectPos;
 
     private void Awake()
     {
@@ -30,10 +31,9 @@ public class RightClickMenu : MonoBehaviour, IPointerClickHandler, IClickState
     {
         if (pointerEventData.button.Equals(PointerEventData.InputButton.Right))
         {
-            rightClickMenu.transform.position = new Vector2(pointerEventData.position.x + 150f, pointerEventData.position.y - 50f);
-            Debug.Log(pointerEventData.position);
-            rightClickMenu.SetActive(true);
             OnPointerObject();
+            rightClickMenu.transform.position = new Vector2(pointerEventData.position.x + 150f, pointerEventData.position.y - 50f);
+            rightClickMenu.SetActive(true);
         } else if (pointerEventData.button.Equals(PointerEventData.InputButton.Left))
         {
             rightClickMenu.SetActive(false);
@@ -56,6 +56,7 @@ public class RightClickMenu : MonoBehaviour, IPointerClickHandler, IClickState
     { // Raycast Point를 통해 오브젝트 목록 가져오기
         
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // ray의 좌표와 클릭 시 좌표를 일치시키면 될듯...
+        Debug.DrawRay(ray.origin, ray.direction * 100f, Color.red);
         RaycastHit[] hit;
 
         hit = Physics.RaycastAll(ray);
@@ -63,10 +64,16 @@ public class RightClickMenu : MonoBehaviour, IPointerClickHandler, IClickState
         {
             for (int i = 0; i < hit.Length; i++)
             { // 마우스 클릭 시 좌표와 태그가 있는 오브젝트 사이 간 거리가 제일 가까운 오브젝트와 상호작용 하기 위한 비교 과정
-                if (hit[i].transform.gameObject.CompareTag("Untagged")) continue; // 상호작용 안되는 오브젝트 추가 예정
-                objectDistance = Vector2.Distance(Input.mousePosition, hit[i].transform.position);
-
-                Debug.Log(hit[i].transform.position);
+                if (hit[i].collider.CompareTag("Untagged")) continue; // 상호작용 안되어야하는 오브젝트 추가 예정
+                if (hit.Length > 1)
+                {
+                    objectPos = hit[i].collider.gameObject.transform.parent;
+                } else
+                {
+                    objectPos = hit[i].collider.gameObject.transform;
+                }
+                objectDistance = Vector2.Distance(Input.mousePosition, objectPos.position);
+                Debug.Log($"{hit[i].collider.gameObject.name}'s Hit Position: {hit[i].collider.gameObject.transform.position}");
                 if (minDistance == 0 || minDistance > objectDistance)
                 {
                     // minDistance == 0은 처음, 이후 min과 object distance를 비교
@@ -76,7 +83,7 @@ public class RightClickMenu : MonoBehaviour, IPointerClickHandler, IClickState
             }
         }
 
-        Debug.Log($"{minHit.transform.tag} Hit: {minHit.transform.position}");
+        Debug.Log($"Min Position: {minHit.collider.tag}");
         // minHit에 대한 메뉴 출력... todo
         // Player Tag는 쉬는 것
         // Door Tag 열기
@@ -84,14 +91,15 @@ public class RightClickMenu : MonoBehaviour, IPointerClickHandler, IClickState
         // 메뉴 개수
         if (minHit.transform.gameObject.CompareTag("Player"))
         {
+            PlayerClick();
+        } else if (minHit.transform.gameObject.CompareTag("Door"))
+        {
             DoorClick();
         }
     }
 
-    #region Click State
-    public void DoorClick()
+    private void ClickMenuLoad(string[] menu)
     {
-        string[] menu = { "menu1", "menu2", "menu3" }; // Text, 오브젝트마다 달라짐
         buttonCount = menu.Length;
         //  button 갖고와서 butto text 변경해주기
         for (int i = 0; i < buttonCount; i++)
@@ -102,6 +110,19 @@ public class RightClickMenu : MonoBehaviour, IPointerClickHandler, IClickState
             rightClickButtons[i].buttonText.text = menu[i];
         }
         MenuSizeUpdate(buttonCount);
+    }
+
+    #region Click State
+    public void PlayerClick()
+    {
+        string[] menu = { "Player1", "Player2", "Player3" }; // Text, 오브젝트마다 달라짐
+        ClickMenuLoad(menu);
+    }
+
+    public void DoorClick()
+    {
+        string[] menu = { "Door1", "Door2", "Door3", "Door4" }; // Text, 오브젝트마다 달라짐
+        ClickMenuLoad(menu);
     }
     #endregion
 }
