762f20769043f0d25c06e581bb3e61307abb11dc
Sujin <tnwlsqp12@gmail.com>
2023-11-20 13:59:04 +0900

    [Feat] Map_Item_Random, Window/Door,Curtain _fix

--- Assets/3.Script/ECT/Map/RoomGenerator.cs
+++ Assets/3.Script/ECT/Map/RoomGenerator.cs
@@ -1,6 +1,26 @@
 ﻿using System.Collections.Generic;
+using UnityEngine;
+
 public class RoomGenerator
 {
+    private int maxIteration;
+    private int roomLengthMin;
+    private int roomWidthMin;
 
-    
+    public RoomGenerator(int maxIteration, int roomLengthMin, int roomWidthMin)
+    {
+        this.maxIteration = maxIteration;
+        this.roomLengthMin = roomLengthMin;
+        this.roomWidthMin = roomWidthMin;
+    }
+   /* public List<RoomNode> GenerateRoomsInGivenSpace(List<Node> roomSpace)
+    {
+        List<RoomNode> listToReturn = new List<RoomNode>();
+        foreach (var space in roomSpace)
+        {
+            Vector2Int newBottomLeftPoint = StructureHelper.GenerateBottomLeftCornerBetween(space.BottomLeftAreaCorner, space.TopRightAreaCorner, 0.1f, 1);
+        };
+
+
+    }*/
 }
