93e94c22ff19a68e6305551e56fc75e29373be5f
BANIKIM <72877425+BANIKIM@users.noreply.github.com>
2023-11-20 09:30:22 +0900

    [Feat] 창문넘기 코드 수정 E키 누르고 있으면 넘어가게 수정

--- Assets/3.Script/Player/Player_Window_Jump.cs
+++ Assets/3.Script/Player/Player_Window_Jump.cs
@@ -6,7 +6,7 @@ public class Player_Window_Jump : MonoBehaviour
 {
     [Header("플레이어를 넣으세요")]
     [SerializeField] private Player_Move player;
-
+    float keydown = 0f;
     private void Start()
     {
         player = GetComponentInParent<Player_Move>();
@@ -17,23 +17,31 @@ public class Player_Window_Jump : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Window"))
         {
-            if (Input.GetKeyDown(KeyCode.E))//창문이 열려있는지 확인이 필요
+            if (Input.GetKey(KeyCode.E))//창문이 열려있는지 확인이 필요
             {
-                player.animator.SetTrigger("isClimbing");
-                if (player.transform.rotation.y < 0)//WD방향
+                keydown += Time.deltaTime;
+                Debug.Log(keydown);
+                if (keydown >= 0.8f)
                 {
-                    player.transform.position = other.transform.position + new Vector3(-1f, 1f, 1f);
-                    Debug.Log("WD방향");
-                    Debug.Log(player.transform.rotation.y);
-                }
-                else if (player.transform.rotation.y > 0)//S방향
-                {
-                    player.transform.position = other.transform.position + new Vector3(-0.5f, 0, -0.5f);
-                    Debug.Log("S방향");
-                    Debug.Log(player.transform.rotation.y);
-                }
-                
+                    player.animator.SetTrigger("isClimbing");
+                    if (player.transform.rotation.y < 0)//WD방향
+                    {
+                        player.transform.position = other.transform.position + new Vector3(-1f, 1f, 1f);
+                        Debug.Log("WD방향");
+                        Debug.Log(player.transform.rotation.y);
+                    }
+                    else if (player.transform.rotation.y > 0)//S방향
+                    {
+                        player.transform.position = other.transform.position + new Vector3(-0.5f, 0, -0.5f);
+                        Debug.Log("S방향");
+                        Debug.Log(player.transform.rotation.y);
+                    }
 
+                }
+            }
+            else
+            {
+                keydown = 0f;
             }
 
         }
