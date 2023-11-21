4f3211ff690a9aeae2538b49609b6d442702bca2
Sujin <tnwlsqp12@gmail.com>
2023-11-21 13:29:36 +0900

    [Feat] Map_organize

---
+++ Assets/3.Script/Map/DungeonCreator.cs
@@ -0,0 +1,177 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonCreator : MonoBehaviour
+{
+    public int dungeonWidth, dungeonLength;
+    public int roomWidthMin, roomLengthMin;
+    public int maxIterations;
+    public int corridorWidth;
+    public Material material;
+
+    [Range(0.0f, 0.3f)]
+    public float roomBottomCornerModifier;      //바닥 모서리
+    [Range(0.7f, 1.0f)]
+    public float roomTopCornerMidifier;
+    [Range(0, 2)]
+    public int roomOffset;
+
+    /*public GameObject wallVertical, wallHorizontal;
+    List<Vector3Int> possibleDoorVerticalPosition;
+    List<Vector3Int> possibleDoorHorizontalPosition;
+    List<Vector3Int> possibleWallHorizontalPosition;
+    List<Vector3Int> possibleWallVerticalPosition;*/
+
+    public GameObject HousePrefabs;
+
+    void Start()
+    {
+        CreateDungeon();
+    }
+
+    public void CreateDungeon()
+    {
+        DestroyAllChildren();          //던전 생성 전에 기존에 생성된 자식 객체들을 모두 제거
+
+        DugeonGenerator generator = new DugeonGenerator(dungeonWidth, dungeonLength);
+
+        // 던전 공간 정보 리스트로 받아오는 메서드 호출
+        var listOfRooms = generator.CalculateDungeon(
+            maxIterations,
+            roomWidthMin,
+            roomLengthMin,
+            roomBottomCornerModifier,
+            roomTopCornerMidifier,
+            roomOffset,
+            corridorWidth);
+
+        // 벽을 담을 부모 객체 생성
+        GameObject wallParent = new GameObject("WallParent");
+        wallParent.transform.parent = transform;
+        //possibleDoorVerticalPosition = new List<Vector3Int>();
+        //possibleDoorHorizontalPosition = new List<Vector3Int>();
+        //possibleWallHorizontalPosition = new List<Vector3Int>();
+        //possibleWallVerticalPosition = new List<Vector3Int>();
+
+        for (int i = 0; i < listOfRooms.Count; i++)  //각 방에 대한 메쉬를 생성하는 메서드 호출
+        {
+            CreateMesh(listOfRooms[i].BottomLeftAreaCorner, listOfRooms[i].TopRightAreaCorner);
+        }
+        //CreateWalls(wallParent);
+    }
+
+   /* private void CreateWalls(GameObject wallParent)
+    {
+        foreach (var wallPosition in possibleWallHorizontalPosition)
+        {
+            CreateWall(wallParent, wallPosition, wallHorizontal);
+        }
+        foreach (var wallPosition in possibleWallVerticalPosition)
+        {
+            CreateWall(wallParent, wallPosition, wallVertical);
+        }
+    }
+
+    private void CreateWall(GameObject wallParent, Vector3Int wallPosition, GameObject wallPrefab)
+    {
+        Instantiate(wallPrefab, wallPosition, Quaternion.identity, wallParent.transform);
+    }*/
+
+    private void CreateMesh(Vector2 bottomLeftCorner, Vector2 topRightCorner)
+    {
+        Vector3 bottomLeftV = new Vector3(bottomLeftCorner.x, 0, bottomLeftCorner.y);
+        Vector3 bottomRightV = new Vector3(topRightCorner.x, 0, bottomLeftCorner.y);
+        Vector3 topLeftV = new Vector3(bottomLeftCorner.x, 0, topRightCorner.y);
+        Vector3 topRightV = new Vector3(topRightCorner.x, 0, topRightCorner.y);
+
+        // 메쉬를 위한 정점 및 UV 생성
+        Vector3[] vertices = new Vector3[]
+        {
+            topLeftV,
+            topRightV,
+            bottomLeftV,
+            bottomRightV
+        };
+
+        Vector2[] uvs = new Vector2[vertices.Length]; 
+        for (int i = 0; i < uvs.Length; i++)
+        {
+            uvs[i] = new Vector2(vertices[i].x, vertices[i].z);
+        }
+
+        // 삼각형 인덱스 생성
+        int[] triangles = new int[]
+        {
+            0,
+            1,
+            2,
+            2,
+            1,
+            3
+        };
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.uv = uvs;
+        mesh.triangles = triangles;
+
+        // 던전 바닥 객체 생성
+        GameObject dungeonFloor = new GameObject("Mesh" + bottomLeftCorner, typeof(MeshFilter), typeof(MeshRenderer));
+
+        // 위치 및 크기 설정
+        dungeonFloor.transform.position = Vector3.zero;
+        dungeonFloor.transform.localScale = Vector3.one;
+        dungeonFloor.GetComponent<MeshFilter>().mesh = mesh;
+        dungeonFloor.GetComponent<MeshRenderer>().material = material;
+        dungeonFloor.transform.parent = transform;
+
+/*        for (int row = (int)bottomLeftV.x; row < (int)bottomRightV.x; row++)
+        {
+            var wallPosition = new Vector3(row, 0, bottomLeftV.z);
+            AddWallPositionToList(wallPosition, possibleWallHorizontalPosition, possibleDoorHorizontalPosition);
+        }
+        for (int row = (int)topLeftV.x; row < (int)topRightCorner.x; row++)
+        {
+            var wallPosition = new Vector3(row, 0, topRightV.z);
+            AddWallPositionToList(wallPosition, possibleWallHorizontalPosition, possibleDoorHorizontalPosition);
+        }
+        for (int col = (int)bottomLeftV.z; col < (int)topLeftV.z; col++)
+        {
+            var wallPosition = new Vector3(bottomLeftV.x, 0, col);
+            AddWallPositionToList(wallPosition, possibleWallVerticalPosition, possibleDoorVerticalPosition);
+        }
+        for (int col = (int)bottomRightV.z; col < (int)topRightV.z; col++)
+        {
+            var wallPosition = new Vector3(bottomRightV.x, 0, col);
+            AddWallPositionToList(wallPosition, possibleWallVerticalPosition, possibleDoorVerticalPosition);
+        }*/
+    }
+
+
+    // 벽 또는 문의 위치를 리스트에 추가
+    /*private void AddWallPositionToList(Vector3 wallPosition, List<Vector3Int> wallList, List<Vector3Int> doorList)
+    {
+        Vector3Int point = Vector3Int.CeilToInt(wallPosition);
+        if (wallList.Contains(point))
+        {
+            doorList.Add(point);
+            wallList.Remove(point);
+        }
+        else
+        {
+            wallList.Add(point);
+        }
+    }*/
+
+    private void DestroyAllChildren()
+    {
+        while (transform.childCount != 0)
+        {
+            foreach (Transform item in transform)
+            {
+                DestroyImmediate(item.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/3.Script/Map/StructureHelper.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public static class StructureHelper   //그래프 탐색 및 벡터 연산
+{
+
+    //주어진 부모 노드에서 최하위 잎 노드를 추출하여 반환하는 메서드
+    public static List<Node> TraverseGraphToExtractLowestLeafes(Node parentNode)
+    {
+        Queue<Node> nodesToCheck = new Queue<Node>();
+        List<Node> listToReturn = new List<Node>();
+
+        //부모 노드가 잎 노드인 경우 해당 노드를 반환
+        if (parentNode.ChildrenNodeList.Count == 0)
+        {
+            return new List<Node>() { parentNode };
+        }
+
+        //부모 노드의 자식 노드들을 큐에 추가
+        foreach (var child in parentNode.ChildrenNodeList)
+        {
+            nodesToCheck.Enqueue(child);
+        }
+
+        // 큐를 이용한 BFS 탐색
+        while (nodesToCheck.Count > 0)
+        {
+            var currentNode = nodesToCheck.Dequeue();
+            if (currentNode.ChildrenNodeList.Count == 0)
+            {
+                listToReturn.Add(currentNode);
+            }
+            else
+            {
+                foreach (var child in currentNode.ChildrenNodeList)
+                {
+                    nodesToCheck.Enqueue(child);
+                }
+            }
+        }
+        return listToReturn;
+    }
+
+    public static Vector2Int GenerateBottomLeftCornerBetween(
+        Vector2Int boundaryLeftPoint, Vector2Int boundaryRightPoint, float pointModifier, int offset)
+    {
+        int minX = boundaryLeftPoint.x + offset;
+        int maxX = boundaryRightPoint.x - offset;
+        int minY = boundaryLeftPoint.y + offset;
+        int maxY = boundaryRightPoint.y - offset;
+        return new Vector2Int(
+            Random.Range(minX, (int)(minX + (maxX - minX) * pointModifier)),
+            Random.Range(minY, (int)(minY + (minY - minY) * pointModifier)));
+    }
+
+    public static Vector2Int GenerateTopRightCornerBetween(
+        Vector2Int boundaryLeftPoint, Vector2Int boundaryRightPoint, float pointModifier, int offset)
+    {
+        int minX = boundaryLeftPoint.x + offset;
+        int maxX = boundaryRightPoint.x - offset;
+        int minY = boundaryLeftPoint.y + offset;
+        int maxY = boundaryRightPoint.y - offset;
+        return new Vector2Int(
+            Random.Range((int)(minX + (maxX - minX) * pointModifier), maxX),
+            Random.Range((int)(minY + (maxY - minY) * pointModifier), maxY)
+            );
+    }
+
+    public static Vector2Int CalculateMiddlePoint(Vector2Int v1, Vector2Int v2)
+    {
+        Vector2 sum = v1 + v2;
+        Vector2 tempVector = sum / 2;
+        return new Vector2Int((int)tempVector.x, (int)tempVector.y);
+    }
+}
+
+public enum RelativePosition
+{
+    Up,
+    Down,
+    Right,
+    Left
+}
