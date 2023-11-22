9f231af3e231f3b591a807720ae2cbee451243a7
BANIKIM <72877425+BANIKIM@users.noreply.github.com>
2023-11-22 09:10:59 +0900

    [Feat] 저장전 레벨체크 기능 추가

--- Assets/3.Script/Player/Player_Level_Up.cs
+++ Assets/3.Script/Player/Player_Level_Up.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player_Level_Up : MonoBehaviour
 {
+    int level = 0;
+
+    //레벨업기능
     public void Level_up(GameObject[] gameObjects)
     {
 
@@ -18,8 +21,24 @@ public class Player_Level_Up : MonoBehaviour
                 gameObjects[i].transform.GetChild(1).gameObject.SetActive(true);
                 break;
             }
+        }         
+    }
+
+    //레벨체크기능
+    public int Level_check(GameObject[] gameObjects)
+    {
+        for (int i = 0; i < gameObjects.Length; i++)
+        {
+            if (gameObjects[i].transform.GetChild(1).gameObject.activeSelf == true)
+            {
+
+            }
+            else
+            {
+                level = i;
+                break;                               
+            }
         }
-  
-         
+        return level;
     }
 }
